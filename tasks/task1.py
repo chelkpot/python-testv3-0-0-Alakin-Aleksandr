1181202c7bf23cd09f8a003f38527436ec58c6b0
Alakin-Aleksandr <alakinsasa2008@gmail.com>
2025-10-09 09:10:27 +0500

    Add files via upload
    Переделал

--- tasks/task1.py
+++ tasks/task1.py
@@ -2,12 +2,12 @@
 
 def solve():
 # Ниже пишите решение задачи
-    num= int(input("Введите число: "))
+    num= int(input())
     num_last_digit= num%10
     num_second_digit= num//10%10
     num_first_digit= num//100%10
-    sum= num_last_digit + num_second_digit + num_first_digit
-    print(sum)
+    n= num_last_digit + num_second_digit + num_first_digit
+    print(n)
 
     
 # Код ниже не трогать! он нужен для тестов
